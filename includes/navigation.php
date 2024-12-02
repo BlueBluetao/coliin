38ee4ddb1867b4fde7295a758eebaf58c8360820
Blue Kong/孔布鲁 <blue.kong@interlyst.co>
2024-12-02 22:31:34 +0800

    add

--- includes/navigation.php
+++ includes/navigation.php
@@ -38,31 +38,27 @@
     <div class="main-header md-hidden sm-hidden">
         <div class="container-custom">
             <div class="row">
-                <div class="col-md-3">
-                    <div class="logo-brand">
-                        <a href="index.php"><img src="images/logo.png" alt="<?php echo SITE_NAME; ?>"></a>
-                    </div>
+                <div class="logo-brand">
+                    <a href="index.php"><img src="images/logo.png" alt="<?php echo SITE_NAME; ?>"></a>
                 </div>
-                <div class="col-md-9">
-                    <div class="main-navigation">
-                        <ul id="primary-menu" class="menu">
-                            <?php foreach ($main_menu as $item): ?>
-                                <li class="menu-item <?php echo isset($item['submenu']) ? 'menu-item-has-children' : ''; ?> <?php echo isset($item['current']) && $item['current'] ? 'current-menu-ancestor current-menu-parent' : ''; ?>">
-                                    <a href="<?php echo $item['url']; ?>"><?php echo $item['title']; ?></a>
-                                    <?php if (isset($item['submenu'])): ?>
-                                        <ul class="sub-menu">
-                                            <?php foreach ($item['submenu'] as $submenu): ?>
-                                                <li class="menu-item">
-                                                    <a href="<?php echo $submenu['url']; ?>"><?php echo $submenu['title']; ?></a>
-                                                </li>
-                                            <?php endforeach; ?>
-                                        </ul>
-                                    <?php endif; ?>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                        <a href="get-a-quote.php" class="btn btn-primary">Get a quote<i class="icon ion-md-paper-plane"></i></a>
-                    </div>
+                <div class="main-navigation">
+                    <ul id="primary-menu" class="menu">
+                        <?php foreach ($main_menu as $item): ?>
+                            <li class="menu-item <?php echo isset($item['submenu']) ? 'menu-item-has-children' : ''; ?> <?php echo isset($item['current']) && $item['current'] ? 'current-menu-ancestor current-menu-parent' : ''; ?>">
+                                <a href="<?php echo $item['url']; ?>"><?php echo $item['title']; ?></a>
+                                <?php if (isset($item['submenu'])): ?>
+                                    <ul class="sub-menu">
+                                        <?php foreach ($item['submenu'] as $submenu): ?>
+                                            <li class="menu-item">
+                                                <a href="<?php echo $submenu['url']; ?>"><?php echo $submenu['title']; ?></a>
+                                            </li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                <?php endif; ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <a href="get-a-quote.php" class="btn btn-primary">Get a quote<i class="icon ion-md-paper-plane"></i></a>
                 </div>
             </div>
         </div>
